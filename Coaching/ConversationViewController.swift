67a0ec37b944ab1fb85931375611556780980f39
Jason Pierna <siilver777@me.com>
2018-02-18 15:54:27 +0100

    Deletes storyboard fully

--- Coaching/ConversationViewController.swift
+++ Coaching/ConversationViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConversationViewController: UIViewController {
 	
 	lazy var scrollView: UIScrollView = {
 		let scrollView = UIScrollView()
