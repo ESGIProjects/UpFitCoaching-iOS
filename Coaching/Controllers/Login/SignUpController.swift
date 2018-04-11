40d6fef4aa841bc91a51f1af346c8cc3d71e4909
Jason Pierna <siilver777@me.com>
2018-04-11 17:57:31 +0200

    Calling register API

--- Coaching/Controllers/Login/SignUpController.swift
+++ Coaching/Controllers/Login/SignUpController.swift
@@ -24,7 +24,7 @@ class SignUpController: UIViewController {
 		
 		title = "signupController_title".localized
 		view.backgroundColor = .white
-		setupLayout()		
+		setupLayout()
 	}
 	
 	override func viewWillAppear(_ animated: Bool) {
@@ -47,6 +47,50 @@ class SignUpController: UIViewController {
 	}
 	
 	@objc func signUp(_ sender: UIButton) {
+		guard let firstName = firstNameTextField.text, firstName != "" else {
+			present(UIAlertController.simpleAlert(title: "firstName_missing_title", message: nil), animated: true)
+			return
+		}
 		
+		guard let lastName = lastNameTextField.text, lastName != "" else {
+			present(UIAlertController.simpleAlert(title: "lastName_missing_title", message: nil), animated: true)
+			return
+		}
+		
+		guard let mail = mailTextField.text, mail != "" else {
+			present(UIAlertController.simpleAlert(title: "mail_missing_title", message: nil), animated: true)
+			return
+		}
+		
+		guard let password = passwordTextField.text, password != "" else {
+			present(UIAlertController.simpleAlert(title: "password_missing_title", message: nil), animated: true)
+			return
+		}
+		
+		guard let confirmPassword = confirmPasswordTextField.text, password == confirmPassword else {
+			present(UIAlertController.simpleAlert(title: "confirmPassword_error_title", message: nil), animated: true)
+			return
+		}
+		
+		Network.register(mail: mail, password: password, type: 1, firstName: firstName, lastName: lastName, birthDate: "1995-08-07", city: "Paris", phoneNumber: "118218") { [weak self] data, response, _ in
+			
+			guard let response = response as? HTTPURLResponse,
+				let data = data else { return }
+			
+			print("Status code:", response.statusCode)
+			let decoder = JSONDecoder()
+			
+			if response.statusCode == 201 {
+				
+			} else {
+				guard let errorMessage = try? decoder.decode(ErrorMessage.self, from: data) else { return }
+				
+				let alertController = UIAlertController.simpleAlert(title: "error".localized, message: errorMessage.message.localized)
+				
+				DispatchQueue.main.async {
+					self?.present(alertController, animated: true)
+				}
+			}
+		}
 	}
 }
