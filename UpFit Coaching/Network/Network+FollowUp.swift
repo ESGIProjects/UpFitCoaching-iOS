74d408e79b8972d29091d24581fc1c3d2312a3db
Jason Pierna <siilver777@me.com>
2018-06-24 12:19:44 +0200

    Adds Network POST methods for follow-up

--- UpFit Coaching/Network/Network+FollowUp.swift
+++ UpFit Coaching/Network/Network+FollowUp.swift
@@ -18,6 +18,26 @@ extension Network {
 		call(url, httpMethod: .get, parameters: parameters, completion: completion)
 	}
 	
+	class func postAppraisal(_ appraisal: Appraisal, completion: @escaping NetworkCallback) {
+		let url = baseURL.appending("/appraisals/")
+		
+		let dateFormatter = DateFormatter.time
+		
+		let parameters: [String: Any] = [
+			"userId": appraisal.user.userID,
+			"date": dateFormatter.string(from: appraisal.date),
+			"goal": appraisal.goal,
+			"sessionsByWeek": appraisal.sessionsByWeek,
+			"contraindication": appraisal.contraindication,
+			"sportAntecedents": appraisal.sportAntecedents,
+			"helpNeeded": appraisal.helpNeeded,
+			"hasNutritionist": appraisal.hasNutritionist,
+			"comments": appraisal.comments
+		]
+		
+		call(url, httpMethod: .post, parameters: parameters, completion: completion)
+	}
+	
 	class func getMeasurements(for user: User, completion: @escaping NetworkCallback) {
 		let url = baseURL.appending("/measurements/")
 		let parameters: [String: Any] = [
@@ -27,6 +47,25 @@ extension Network {
 		call(url, httpMethod: .get, parameters: parameters, completion: completion)
 	}
 	
+	class func postMeasurements(_ measurements: Measurements, completion: @escaping NetworkCallback) {
+		let url = baseURL.appending("/measurements/")
+		
+		let dateFormatter = DateFormatter.time
+		
+		let parameters: [String: Any] = [
+			"userId": measurements.user.userID,
+			"date": dateFormatter.string(from: measurements.date),
+			"weight": measurements.weight,
+			"height": measurements.height,
+			"hipCircumference": measurements.hipCircumference,
+			"waistCircumference": measurements.waistCircumference,
+			"thighCircumference": measurements.thighCircumference,
+			"armCircumference": measurements.armCircumference
+		]
+		
+		call(url, httpMethod: .post, parameters: parameters, completion: completion)
+	}
+	
 	class func getTests(for user: User, completion: @escaping NetworkCallback) {
 		let url = baseURL.appending("/tests/")
 		let parameters: [String: Any] = [
@@ -34,5 +73,27 @@ extension Network {
 		]
 		
 		call(url, httpMethod: .get, parameters: parameters, completion: completion)
+	}
+	
+	class func postTest(_ test: Test, completion: @escaping NetworkCallback) {
+		let url = baseURL.appending("/tests/")
+		
+		let dateFormatter = DateFormatter.time
+		
+		let parameters: [String: Any] = [
+			"userId": test.user.userID,
+			"date": dateFormatter.string(from: test.date),
+			"warmUp": test.warmUp,
+			"startSpeed": test.startSpeed,
+			"increase": test.increase,
+			"frequency": test.frequency,
+			"kneeFlexibility": test.kneeFlexibility.rawValue,
+			"shinFlexibility": test.shinFlexibility.rawValue,
+			"hitFootFlexibility": test.hitFootFlexibility.rawValue,
+			"closedFistGroundFlexibility": test.closedFistGroundFlexibility.rawValue,
+			"handFlatGroundFlexibility": test.handFlatGroundFlexibility.rawValue
+		]
+		
+		call(url, httpMethod: .post, parameters: parameters, completion: completion)
 	}
 }
